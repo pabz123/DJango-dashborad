be5ad736f92d02068eb3108f25d6e7e59a915f43
pabz123 <mulungipabire11@gmail.com>
2026-07-02 09:52:49 +0300

    Create superuser on deploy

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ set -e
 
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin1234')" | python manage.py shell
